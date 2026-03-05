8c924ed1e9ee4c3305cbcfcfdcb5350aa4ccea22
alex <alejandrocordobarodriguez@gmail.com>
2026-03-05 15:32:58 +0100

    creo videos.tx

---
+++ client/src/data/videos.ts
@@ -0,0 +1,58 @@
+import v1 from "../img/capturas-de-videos/v1.jpg";
+import v2 from "../img/capturas-de-videos/v2.jpg";
+import v3 from "../img/capturas-de-videos/v3.jpg";
+import v4 from "../img/capturas-de-videos/v4.jpg";
+import v5 from "../img/capturas-de-videos/v5.jpg";
+import v6 from "../img/capturas-de-videos/v6.jpg";
+import v7 from "../img/capturas-de-videos/v7.jpg";
+import v8 from "../img/capturas-de-videos/v8.jpg";
+import v9 from "../img/capturas-de-videos/v9.jpg";
+import v10 from "../img/capturas-de-videos/v10.jpg";
+import v11 from "../img/capturas-de-videos/v11.jpg";
+
+export const videos = [
+  {
+    thumbnail: v1,
+    url: "https://www.youtube.com/watch?v=8hGj40BqaF0&list=PLgK5b5ssLGftsU_q2W0ka5pnUntpCNmTM&index=4"
+  },
+  {
+    thumbnail: v2,
+    url: "https://www.youtube.com/watch?v=uc42Pwpwczg&list=PLgK5b5ssLGftsU_q2W0ka5pnUntpCNmTM&index=4"
+  },
+  {
+    thumbnail: v3,
+    url: "https://www.youtube.com/watch?v=o_AlX2QBy5g&list=PLgK5b5ssLGftsU_q2W0ka5pnUntpCNmTM&index=5"
+  },
+  {
+    thumbnail: v4,
+    url: "https://www.youtube.com/watch?v=lL_fAOY-rUE&list=PLgK5b5ssLGftsU_q2W0ka5pnUntpCNmTM&index=6"
+  },
+  {
+    thumbnail: v5,
+    url: "https://www.youtube.com/watch?v=f6ntdZTjsVo&list=PLgK5b5ssLGftsU_q2W0ka5pnUntpCNmTM&index=6"
+  },
+  {
+    thumbnail: v6,
+    url: "https://www.youtube.com/watch?v=ME0vvC13Ij4&list=PLgK5b5ssLGftsU_q2W0ka5pnUntpCNmTM&index=8"
+  },
+  {
+    thumbnail: v7,
+    url: "https://www.youtube.com/watch?v=nc_974q2Lhw&list=PLgK5b5ssLGftsU_q2W0ka5pnUntpCNmTM&index=9"
+  },
+  {
+    thumbnail: v8,
+    url: "https://www.youtube.com/watch?v=zoGKKxML4to&list=PLgK5b5ssLGftsU_q2W0ka5pnUntpCNmTM&index=10"
+  },
+  {
+    thumbnail: v9,
+    url: "https://www.youtube.com/watch?v=nCcu6gdwq78&list=PLgK5b5ssLGftsU_q2W0ka5pnUntpCNmTM&index=10"
+  },
+  {
+    thumbnail: v10,
+    url: "https://www.youtube.com/watch?v=NQDleY3BVcU&list=PLgK5b5ssLGftsU_q2W0ka5pnUntpCNmTM&index=11"
+  },
+  {
+    thumbnail: v11,
+    url: "https://www.youtube.com/watch?v=l89Kx9oUz-c&list=PLgK5b5ssLGftsU_q2W0ka5pnUntpCNmTM&index=13"
+  }
+];
